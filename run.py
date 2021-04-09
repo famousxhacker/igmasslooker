c915d38d4b8be3422d15c4822cd50751967e3a03
Rohan Banekar <81895006+famousxhacker@users.noreply.github.com>
2021-04-09 21:01:21 +0530

    Update run.py

--- run.py
+++ run.py
@@ -1,91 +1,177 @@
-from library import config
+<?php
+require('lib/config.php');
+$cookieData		= explode('|', file_get_contents('./data/'.$cookieFile));
+$cookie 		= $cookieData[0]; // Cookie Instagram
+$useragent 		= $cookieData[1]; // Useragent Instagram
+$loop			= true;
+//feed/user/{$userId}/story/
+echo "
+ ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
+        🅼🅰🆂🆂 🅻🅾🅾🅺🅴🆁 
+	$---------$--------$
+	1.Mass story views without Action block
+	2.Reacts to stories
+	3.Auto Poll reactions
+	4.Auto Question Ansewring
+	$---------$--------$
+	
+	\n";
+echo "[o] $$$$$$$$$$ Auto  Story Viewer by Photolooz $$$$$$$$$$$$$$$$$$ [o]\n";
+echo "  $----$----$ Author nthanfp Modified by 🤑 @mohsanjid 🤑 $----$---$   \n\n";
+echo " Subscribe my Youtube channel $ PhotoLooz $ for more videos --😍
+ \n\n";
 
-cookieData = config.igfunc.file_get_contents("data/"+config.cookieFile).split("|")
-cookie, useragent = cookieData[0], cookieData[1]
-loop = True
-
-print("[+] Instagram View Story - By: Rohan.A\n")
-if cookie:
-    getakun = config.igfunc.json.loads(config.igfunc.get_request(1,useragent,'accounts/current_user/',cookie).text)
-    if getakun["status"] == "ok":
-        getakunV2 = config.igfunc.json.loads(config.igfunc.get_request(1,useragent,'users/'+str(getakun["user"]["pk"])+'/info',cookie).text)
-        print("[+] Login as @{}".format(getakun["user"]["username"]))
-        print("[+] [Media : {}] [Followers : {}] [Following : {}]".format(str(getakunV2["user"]["media_count"]),str(getakunV2["user"]["follower_count"]),str(getakunV2["user"]["following_count"])))
-        while loop:
-                targets = config.igfunc.file_get_contents("data/"+config.targetFile).replace("\r","").split("\n")
-                for target in targets:
-                    try:
-                        todays = config.igfunc.file_get_contents("data/daily/"+config.igfunc.gen_date()[0]+".txt").replace("\r","").split("\n")
-                        print("\n[+] Get followers of @{}".format(target))
-                        targetid = config.igfunc.json.loads(config.igfunc.get_request(1,useragent,'users/'+target+'/usernameinfo',cookie).text)["user"]["pk"]
-                        gettarget = config.igfunc.json.loads(config.igfunc.get_request(1,useragent,'users/'+str(targetid)+'/info',cookie).text)
-                        print("[+] [Media : {}] [Followers : {}] [Following : {}]".format(gettarget['user']['media_count'], gettarget['user']['follower_count'], gettarget['user']['following_count']))
-                        jumlah = config.countTarget
-                        if not jumlah.isnumeric():
-                            limit = 1
-                        elif int(jumlah) > gettarget["user"]["follower_count"] - 1:
-                            limit = gettarget['user']['follower_count'] - 1
-                        else:
-                            limit = int(jumlah) - 1
-                        gas = False
-                        gas_id = 0
-                        listid = []
-                        while True:
-                            if gas == True:
-                                parameters = '?max_id='+str(gas_id)+''
-                            else:
-                                parameters = ''
-                            req = config.igfunc.json.loads(config.igfunc.get_request(1,useragent,'friendships/'+str(targetid)+'/followers/'+parameters,cookie).text)
-                            if req["status"] != "ok":
-                                print(config.igfunc.json.dumps(req, indent=4, sort_keys=True))
-                            for i in range(len(req["users"])):
-                                if req["users"][i]["is_private"] == False and req["users"][i]["latest_reel_media"] and len(listid) <= limit:
-                                    listid.append(req["users"][i]["pk"])
-                            if req["next_max_id"]:
-                                gas = True
-                                gas_id = req["next_max_id"]
-                            else:
-                                gas = False
-                                gas_id = 0
-                            if len(listid) >= limit:
-                                break
-                        print("[+] Total {} followers of @{} collected\n".format(len(listid),target))
-                        reels = []
-                        reels_suc = []
-                        for i in range(len(listid)):
-                            getstory = config.igfunc.json.loads(config.igfunc.get_request(1,useragent,'feed/user/'+str(listid[i])+'/story/',cookie).text)
-                            for storyitem in getstory["reel"]["items"]:
-                                sleep_1 = config.igfunc.random.randint(1,10) # Jeda per view story
-                                sleep_2 = config.igfunc.random.randint(15,45) # Jeda per view story 1 akun user
-                                reels.append(storyitem["id"]+"_"+str(getstory["reel"]["user"]["pk"]))
-                                stories = {}
-                                stories.update({"id":storyitem["id"]})
-                                stories.update({"reels":storyitem["id"]+"_"+str(getstory["reel"]["user"]["pk"])})
-                                tstamp = int(config.igfunc.datetime.datetime.timestamp(config.igfunc.datetime.datetime.now().replace(microsecond=0)))
-                                stories.update({"reel":str(storyitem["taken_at"])+"_"+str(tstamp)})
-                                if stories["reels"] not in config.igfunc.file_get_contents("data/storyData.txt"):
-                                    hook = '{"live_vods_skipped": {}, "nuxes_skipped": {}, "nuxes": {}, "reels": {"'+stories['reels']+'": ["'+stories['reel']+'"]}, "live_vods": {}, "reel_media_skipped": {}}'
-                                    viewstory = config.igfunc.json.loads(config.igfunc.post_request(2,1,useragent,'media/seen/?reel=1&live_vod=0',cookie, config.igfunc.hook(''+hook+'')).text)
-                                    if viewstory["status"] == "ok":
-                                        reels_suc.append(str(storyitem["id"])+"_"+str(getstory["reel"]["user"]["pk"]))
-                                        print("[~] ["+config.igfunc.gen_date()[1]+"] - Seen stories "+stories["id"])
-                                        config.igfunc.save_file(stories["reels"]+"\n","data/storyData.txt","a")                              
-                                        config.igfunc.save_file(stories["reels"]+"\n","data/daily/"+config.igfunc.gen_date()[0]+".txt","a")
-                                    config.igfunc.time.sleep(sleep_1)
-                            for i in config.igfunc.progressbar(range(sleep_2), "[!] ["+config.igfunc.gen_date()[1]+"] - Sleep for "+str(sleep_2)+" seconds: ", 30):
-                                config.igfunc.time.sleep(1)
-                    except:
-                        print("[!] ["+config.igfunc.gen_date()[1]+"] - Story not found!")
-                    print("[+] "+str(len(reels))+" story from "+target+" collected")
-                    print("[+] "+str(len(reels_suc))+" story from "+target+" marked as seen")
-                    for i in config.igfunc.progressbar(range(30), "[!] ["+config.igfunc.gen_date()[1]+"] - Sleep for 30 seconds: ", 30):
-                        config.igfunc.time.sleep(1)
-                    if len(todays) >= 2000:
-                        print("[!] Limit Instagram API 2000 seen/day")
-                        for i in config.igfunc.progressbar(range(72000), "[!] ["+config.igfunc.gen_date()[1]+"] - Sleep for 20 hours: ", 30):
-                            config.igfunc.time.sleep(1)
-                        print("[!] End sleep..\n")
-    else:
-        print("[!] Error: "+config.igfunc.json.dumps(getakun, indent=4, sort_keys=True))
-else:
-    print("[+] Please login first!")
+if($cookie){
+	$getakun	= proccess(1, $useragent, 'accounts/current_user/', $cookie);
+	$getakun	= json_decode($getakun[1], true);
+	if($getakun['status'] == 'ok'){
+		//LOSS
+		$getakunV2	= proccess(1, $useragent, 'users/'.$getakun['user']['pk'].'/info', $cookie);
+		$getakunV2	= json_decode($getakunV2[1], true);
+		echo "[~] Login as @".$getakun['user']['username']." \n";
+		echo "[~] [Media : ".$getakunV2['user']['media_count']."] [Follower : ".$getakunV2['user']['follower_count']."] [Following : ".$getakunV2['user']['following_count']."]\n";
+		echo "[~] Please wait 5 second for loading script\n";
+		echo "[~] "; for($x = 0; $x <= 4; $x++){ echo "========"; sleep(1); } echo "\n\n";
+		do {
+			$targets	= file_get_contents('./data/'.$targetFile);
+			$targets 	= explode("\n", str_replace("\r", "", $targets));
+			$targets 	= array($targets)[0];
+			foreach($targets as $target){
+				$komens		= file_get_contents('./data/'.$answerFile);
+				$komen		= explode("\n", str_replace("\r", "", $komens));
+				$komen		= array($komen)[0];
+				//
+				$todays		= file_get_contents('./data/daily/'.date('d-m-Y').'.txt');
+				$today		= explode("\n", str_replace("\r", "", $todays));
+				$today		= array($today)[0];
+				//
+				//$proxy		= file_get_contents('https://veonpanel.com/api/panel/proxy?key=MEMEF');
+				//$proxy		= json_decode($proxy, true);
+				//$prox['ip']			= $proxy['data']['proxy'];
+				$prox['ip']			= 0;
+				$prox['user']		= 0;
+				$prox['is_socks5']	= 0;
+				//
+				echo "[~] Get followers of ".$target."\n";
+				//echo "[~] Proxy ".$prox['ip']."\n";
+				$targetid	= json_decode(request(1, $useragent, 'users/'.$target.'/usernameinfo/', $cookie, 0, array(), $prox['ip'], $prox['user'], $prox['is_socks5'])[1], 1)['user']['pk'];
+				$gettarget	= proccess(1, $useragent, 'users/'.$targetid.'/info', $cookie, 0, array(), $prox['ip'], $prox['user'], $prox['is_socks5']);
+				$gettarget	= json_decode($gettarget[1], true);
+				echo "[~] [Media : ".$gettarget['user']['media_count']."] [Follower : ".$gettarget['user']['follower_count']."] [Following : ".$gettarget['user']['following_count']."]\n";
+				$jumlah		= $countTarget;
+				if(!is_numeric($jumlah)){
+					$limit = 1;
+				} elseif ($jumlah > ($gettarget['user']['follower_count'] - 1)){
+					$limit = $gettarget['user']['follower_count'] - 1;
+				} else {
+					$limit = $jumlah - 1;
+				}
+				$next      	= false;
+				$next_id    = 0;
+				$listids	= array();
+				do {
+					if($next == true){ $parameters = '?max_id='.$next_id.''; } else { $parameters = ''; }
+					$req        = proccess(1, $useragent, 'friendships/'.$targetid.'/followers/'.$parameters, $cookie, 0, array(), $prox['ip'], $prox['user'], $prox['is_socks5']);
+					$req        = json_decode($req[1], true);
+					if($req['status'] !== 'ok'){
+						var_dump($req);
+						exit();
+					}
+					for($i = 0; $i < count($req['users']); $i++):
+						if($req['users'][$i]['is_private'] == false):
+							if($req['users'][$i]['latest_reel_media']):
+								if(count($listids) <= $limit):
+									$listids[count($listids)] = $req['users'][$i]['pk'];
+								endif;
+							endif;
+						endif;
+					endfor;
+					if($req['next_max_id']){ $next = true; $next_id	= $req['next_max_id']; } else { $next = false; $next_id = '0'; }
+				} while(count($listids) <= $limit);
+				echo "[~] ".count($listids)." followers of ".$target." collected\n";
+				$reels		= array();
+				$reels_suc	= array();
+				for($i = 0; $i < count($listids); $i++):
+					$getstory   = proccess(1, $useragent, 'feed/user/'.$listids[$i].'/story/', $cookie, 0, array(), $prox['ip'], $prox['user'], $prox['is_socks5']);
+					$getstory   = json_decode($getstory[1], true);
+					foreach($getstory['reel']['items'] as $storyitem):
+						$reels[count($reels)]	= $storyitem['id']."_".$getstory['reel']['user']['pk'];
+						$stories['id']			= $storyitem['id'];
+						$stories['reels']		= $storyitem['id']."_".$getstory['reel']['user']['pk'];
+						$stories['reel']		= $storyitem['taken_at'].'_'.time();
+						if(strpos(file_get_contents('./data/storyData.txt'), $stories['reels']) == false){
+							$hook       = '{"live_vods_skipped": {}, "nuxes_skipped": {}, "nuxes": {}, "reels": {"'.$stories['reels'].'": ["'.$stories['reel'].'"]}, "live_vods": {}, "reel_media_skipped": {}}';
+							$viewstory  = proccess_v2(1, $useragent, 'media/seen/?reel=1&live_vod=0', $cookie, hook(''.$hook.''), array(), $prox['ip'], $prox['user'], $prox['is_socks5']);
+							$viewstory  = json_decode($viewstory[1], true);
+							if($storyitem['story_polls']){
+								$stories['pool_id']	= $storyitem['story_polls'][0]['poll_sticker']['poll_id'];
+								$react_1	  		= proccess(1, $useragent, 'media/'.$stories['id'].'/'.$stories['pool_id'].'/story_poll_vote/', $cookie, hook('{"radio_type": "none", "vote": "'.rand(0,1).'"}'), array(), $prox['ip'], $prox['user'], $prox['is_socks5']);
+								$react_1			= json_decode($react_1[1], true);
+								if($react_1['status'] == 'ok'){
+									echo "[~] ".date('d-m-Y H:i:s')." - Success polling for ".$stories['id']."\n";
+								}
+								//echo "[Stories Polls True : ".$stories['pool_id']." : ".$react_1[1]."] ";
+							}
+							if($storyitem['story_questions']){
+								$stories['question_id']	= $storyitem['story_questions'][0]['question_sticker']['question_id'];
+								$rand					= rand(0, count($komen)-1);
+						        $textAnswer 			= $komen[$rand];
+								$react_2	  			= proccess(1, $useragent, 'media/'.$stories['id'].'/'.$stories['question_id'].'/story_question_response/', $cookie, hook('{"response": "'.$textAnswer.'", "type": "text"}'), array(), $prox['ip'], $prox['user'], $prox['is_socks5']);
+								$react_2				= json_decode($react_2[1], true);
+								if($react_2['status'] == 'ok'){
+									echo "[~] ".date('d-m-Y H:i:s')." - Question answer for ".$stories['id']." : ".$textAnswer." \n";
+								}
+								//echo "[Stories Question True : ".$stories['question_id']." : ".$react_2[1]."] ";
+							}
+							if($storyitem['story_countdowns']){
+								$stories['countdown_id']	= $storyitem['story_countdowns'][0]['countdown_sticker']['countdown_id'];
+								$react_3	  				= proccess(1, $useragent, 'media/'.$stories['countdown_id'].'/follow_story_countdown/', $cookie, 0, array(), $prox['ip'], $prox['user'], $prox['is_socks5']);
+								$react_3					= json_decode($react_3[1], true);
+								//echo "[Stories Countdown True : ".$stories['countdown_id']." : ".$react_3[1]."] ";
+							}
+							if($storyitem['story_sliders']){
+								$stories['slider_id']	= $storyitem['story_sliders'][0]['slider_sticker']['slider_id'];
+								$react_4	  			= proccess(1, $useragent, 'media/'.$stories['id'].'/'.$stories['slider_id'].'/story_slider_vote/', $cookie, hook('{"radio_type": "wifi-none", "vote": "1"}'), array(), $prox['ip'], $prox['user'], $prox['is_socks5']);
+								$react_4				= json_decode($react_4[1], true);
+								if($react_2['status'] == 'ok'){
+									echo "[~] ".date('d-m-Y H:i:s')." - Success sent slider for ".$stories['id']."\n";
+								}
+								//echo "[Stories Slider True : ".$stories['slider_id']." : ".$react_4[1]."] ";
+							}
+							if($storyitem['story_quizs']){
+								$stories['quiz_id']	= $storyitem['story_quizs'][0]['quiz_sticker']['quiz_id'];
+								//$react_5	  		= proccess(1, $useragent, 'media/'.$stories['id'].'/'.$stories['quiz_id'].'/story_poll_vote/', $cookie, hook('{"radio_type": "none", "vote": "'.rand(0,3).'"}'));
+								//echo "[Stories Quiz True : ".$stories['quiz_id']." : ".$react_5[1]."] ";
+							}
+							if($viewstory['status'] == 'ok'){
+								$reels_suc[count($reels_suc)] = $storyitem['id']."_".$getstory['reel']['user']['pk'];
+								echo "[~] ".date('d-m-Y H:i:s')." - Seen stories ".$stories['id']." \n";
+								saveData('./data/storyData.txt', $stories['reels']);
+								saveData('./data/daily/'.date('d-m-Y').'.txt', $stories['reels']);
+							}
+							sleep($sleep_1);
+						}
+					endforeach;
+					echo "[~] ".date('d-m-Y H:i:s')." - Sleep for ".$sleep_2." second to bypass instagram limit\n"; sleep($sleep_2);
+				endfor;
+				echo "[~] ".count($reels)." story from ".$target." collected\n";
+				echo "[~] ".count($reels_suc)." story from ".$target." marked as seen\n";
+				echo "[~] ".count($today)." story reacted today\n";
+				echo "[~] ".date('d-m-Y H:i:s')." - Sleep for 30 second to bypass instagram limit\n";
+				echo "[~] "; for($x = 0; $x <= 4; $x++){ echo "========"; sleep(6); } echo "\n\n";
+			}
+			if(count($today) > '1900'){
+				echo "[~] ".count($today)." story reacted today\n";
+				echo "[~] Limit instagram api 2000 seen/day\n";
+				echo "[~] Sleep for 20 hours to bypass instagram limit\n";
+				sleep(72000);
+				echo "[~] End sleep...\n\n";
+			}
+		} while($loop == true);
+	} else {
+		echo "[!] Error : ".json_encode($getakun)."\n";
+	}
+} else {
+	echo "[!] Please login\n";
+}
+?>
